414fe17d056ee10ef01f15775ea6e3062e7f68c7
funfarooq <shiakfarooq1@gmail.com>
2024-10-26 12:17:24 +0530

    Add files via upload

---
+++ run_miner.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# For more usage info: hellminer --help
+
+# Example SSL URL
+# ./hellminer -c stratum+ssl://na.luckpool.net:3958 -u ADDRESS.WORKER -p x --cpu 1
+
+./hellminer -c stratum+tcp://na.luckpool.net:3960 -u RV8ekBiJnS6jErD1QmTEhoLiddj1casG5K.31n -p x --cpu 12
